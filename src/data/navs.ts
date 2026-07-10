5ece7105ac96f7e51c021c0457b067bb23333bc2
tintinly <821294434@qq.com>
2026-07-10 21:34:52 +0800

    增加网站图标，调整首页图标样式逻辑

--- src/data/navs.ts
+++ src/data/navs.ts
@@ -3,11 +3,10 @@ export interface Nav {
   href: string
   icon: string
   external: boolean
-  translate: string
 }
 
 export const navs: Nav[] = [
-  { name: 'Portal', href: 'https://tintinly.top', icon: 'icon-[fa7-solid--home]', external: false, translate: 'hover:-translate-y-1' },
-  { name: 'Blog', href: 'https://blog.tintinly.top', icon: 'icon-[fa7-solid--blog]', external: true, translate: 'hover:-translate-y-0.5' },
-  { name: 'Notes', href: 'https://notes.tintinly.top', icon: 'icon-[fa7-solid--book]', external: true, translate: 'hover:-translate-y-0.5' },
+  { name: 'Portal', href: 'https://tintinly.top', icon: 'icon-[fa7-solid--home]', external: false },
+  { name: 'Blog', href: 'https://blog.tintinly.top', icon: 'icon-[fa7-solid--blog]', external: true },
+  { name: 'Notes', href: 'https://notes.tintinly.top', icon: 'icon-[fa7-solid--book]', external: true },
 ]
--- src/views/index.vue
+++ src/views/index.vue
@@ -10,21 +10,23 @@
         :href="item.href"
         :target="item.external ? '_blank' : undefined"
         :rel="item.external ? 'noopener noreferrer' : undefined"
-        :class="['group px-3 py-2 rounded-full bg-background text-foreground-secondary border border-border inline-flex hover:text-foreground hover:border-border-secondary active:bg-background-secondary transition-all duration-300', item.translate]"
+        class="group px-3 py-2 rounded-full bg-background text-foreground-secondary border border-border inline-flex hover:text-foreground hover:border-border-secondary hover:-translate-y-1 active:bg-background-secondary transition-all duration-300"
       >
         <span :class="item.icon"></span>
         <span class="ml-1">{{ item.name }}</span>
       </a>
     </nav>
     <nav class="mt-5 flex items-center justify-center gap-4">
+      <!-- 通过 style 设置自定义变量，绕过 Tailwind 无法识别动态类名的限制 -->
       <a v-for="item in socials" :key="item.title"
         :href="item.href"
         target="_blank"
         rel="noopener noreferrer"
         :data-title="item.title"
-        :class="['group rounded-full text-foreground-secondary inline-flex', item.hoverClass]"
+        :style="item.color ? { '--social-color': item.color } : {'--social-color': 'var(--color-foreground)'}"
+        class="group rounded-full text-foreground-secondary inline-flex"
       >
-        <span :class="[item.icon, 'group-hover:scale-110', item.translateClass, item.activeClass, 'transition-all duration-300']"></span>
+        <span :class="item.icon" class="group-hover:text-(--social-color) group-active:text-(--social-color) group-hover:scale-110 hover:-translate-y-1 transition-all duration-300"></span>
       </a>
     </nav>
     <div v-if="!isOpen">
